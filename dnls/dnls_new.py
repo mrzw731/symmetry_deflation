7e6b9c84915c6c24c3ad4c039278c6430d500efc
WeiZhu <mrzw731@gmail.com>
2022-07-10 21:43:47 -0400

    add parser

--- dnls/dnls_new.py
+++ dnls/dnls_new.py
@@ -1,4 +1,5 @@
 import numpy as np
+import argparse
 import matplotlib.pyplot as plt
 import copy
 import torch
@@ -10,21 +11,37 @@ import os
 import pdb
 import matplotlib.pyplot as plt
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--C', type=float, default=.1)
+parser.add_argument('--N_sites', type=int, default=4)
+parser.add_argument('--batch_size', type=int, default=500)
+parser.add_argument('--width', type=int, default=400)
+parser.add_argument('--num_Hs', type=int, default=3)
+parser.add_argument('--epochs', type=int, default=1000)
+parser.add_argument('--lr_init', type=float, default=1e-3)
+parser.add_argument('--deflate', type=float, default=.5)
+args = parser.parse_args()
+
 seed = 0
 np.random.seed(seed)
 torch.manual_seed(seed)
 
-C = .1
-N_sites = 4
-batch_size = 500                             # num of training samples per iteration
-width = 400                                  # width of the neural networks representing H's
-num_Hs = 3                                   # number of H's to be learned
-epochs_list = [500, 500, 1000]             # num of epochs to learn each H
+C = args.C
+N_sites = args.N_sites
+batch_size = args.batch_size                # num of training samples per iteration
+width = args.width                          # width of the neural networks representing H's
+num_Hs = args.num_Hs                        # number of H's to be learned
+epochs_list = num_Hs * [args.epochs]        # num of epochs to learn each H
 
-lr_decay_list = [x//2 for x in epochs_list]  # num of epochs after which lr is halfed
-lr_init = 1e-3                               # initial learning rate
+lr_decay_list = [x//2 for x in epochs_list] # num of epochs after which lr is halfed
+lr_init = args.lr_init                      # initial learning rate
 
-deflate = .5                                 # deflation power in the denominator
+deflate = args.deflate                      # deflation power in the denominator
+
+path = './N_sites=%d' % N_sites
+isExist = os.path.exists(path)
+if not isExist:
+    os.makedirs(path)
 
 
 ###################### Define vector field f(z) and z #############################
@@ -34,12 +51,12 @@ def f(x):
 
     abs_uv_sqrd = u**2 + v**2
     # Dirichlet
-    u_dot = -C * torch.cat([v[:,1:2]-2*v[:, 0:1], v[:, :-2]+v[:, 2:]-2*v[:, 1:-1], v[:, -2:-1]-2*v[:, -1:]], dim=1) - abs_uv_sqrd*v
-    v_dot = C * torch.cat([u[:,1:2]-2*u[:, 0:1], u[:, :-2]+u[:, 2:]-2*u[:, 1:-1], u[:, -2:-1]-2*u[:, -1:]], dim=1) + abs_uv_sqrd*u
+    #u_dot = -C * torch.cat([v[:,1:2]-2*v[:, 0:1], v[:, :-2]+v[:, 2:]-2*v[:, 1:-1], v[:, -2:-1]-2*v[:, -1:]], dim=1) - abs_uv_sqrd*v
+    #v_dot = C * torch.cat([u[:,1:2]-2*u[:, 0:1], u[:, :-2]+u[:, 2:]-2*u[:, 1:-1], u[:, -2:-1]-2*u[:, -1:]], dim=1) + abs_uv_sqrd*u
 
     # periodic
-    #u_dot = -C * torch.cat([v[:,1:2]-2*v[:, 0:1]+v[:, -1:], v[:, :-2]+v[:, 2:]-2*v[:, 1:-1], v[:, -2:-1]-2*v[:, -1:]+v[:, :1]], dim=1) - abs_uv_sqrd*v
-    #v_dot = C * torch.cat([u[:,1:2]-2*u[:, 0:1]+u[:, -1:], u[:, :-2]+u[:, 2:]-2*u[:, 1:-1], u[:, -2:-1]-2*u[:, -1:]+u[:, :1]], dim=1) + abs_uv_sqrd*u
+    u_dot = -C * torch.cat([v[:,1:2]-2*v[:, 0:1]+v[:, -1:], v[:, :-2]+v[:, 2:]-2*v[:, 1:-1], v[:, -2:-1]-2*v[:, -1:]+v[:, :1]], dim=1) - abs_uv_sqrd*v
+    v_dot = C * torch.cat([u[:,1:2]-2*u[:, 0:1]+u[:, -1:], u[:, :-2]+u[:, 2:]-2*u[:, 1:-1], u[:, -2:-1]-2*u[:, -1:]+u[:, :1]], dim=1) + abs_uv_sqrd*u
 
     return torch.cat([u_dot, v_dot], dim=1)
 
@@ -185,6 +202,8 @@ def sanity_check(f, models, inputs):
 cuda = torch.device('cuda')
 cpu = torch.device('cpu')
 learned_models = []
+losses_train_all = []
+losses_test_all = []
 
 for idx in range(num_Hs):
     np.random.seed(idx)
@@ -237,9 +256,18 @@ for idx in range(num_Hs):
     plt.plot(losses_test_now)
     plt.legend(['Train loss', 'Test loss'])
     plt.yscale('log')
-    plt.title('Learning H%d, C = %.1f, Num sites = %d' %(idx+1, C, N_sites))
+    plt.title('DNLS, learning H%d, C = %.1f, Num sites = %d' %(idx+1, C, N_sites))
     plt.xlabel('Number of Iterations')
-    plt.savefig('Learning_H%d_N=%d.png' % (idx+1, N_sites))
+    plt.savefig('N_sites=%d/Learning_H%d_N=%d.png' % (N_sites, idx+1, N_sites))
     plt.close()
 
+
+    losses_train_all.append(losses_train_now)
+    losses_test_all.append(losses_test_now)
+    
     learned_models.append(model_now)
+
+losses_train_all = np.array(losses_train_all)
+losses_test_all = np.array(losses_test_all)    
+np.save('N_sites=%d/losses_train_all.npy' % (N_sites), losses_train_all)
+np.save('N_sites=%d/losses_test_all.npy' % (N_sites), losses_test_all)
