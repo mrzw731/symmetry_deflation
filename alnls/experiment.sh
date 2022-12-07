4c7382541722e9785da0b913fb73195eab7d2013
WeiZhu <mrzw731@gmail.com>
2022-12-06 20:20:49 -0500

    involution_alnls and more experiments

--- alnls/experiment.sh
+++ alnls/experiment.sh
@@ -1,4 +1,11 @@
 for N_sites in {3..10} #3 4 5 6 7 8 9 10
 do
-    python alnls_new.py --num_Hs $((2*N_sites)) --N_sites $N_sites --epochs 2000
+    python alnls_involution.py --num_Hs $((2*N_sites)) --N_sites $N_sites --train_box 10000 --epochs 5000 --deflate 1 --width 800
+done
+
+
+
+for N_sites in {3..10} #3 4 5 6 7 8 9 10
+do
+    python alnls_involution.py --num_Hs $((2*N_sites)) --N_sites $N_sites --train_box 10000 --epochs 5000 --deflate .5 --width 800
 done
