d3b8d24e4a61c3d495921403a659cc99f1b2e709
WeiZhu <mrzw731@gmail.com>
2022-12-21 10:21:45 -0500

    change experiments, add plot_figures

--- alnls/experiment.sh
+++ alnls/experiment.sh
@@ -1,5 +1,6 @@
 for N_sites in {3..10} #3 4 5 6 7 8 9 10
 do
+    python alnls_involution.py --num_Hs $((2*N_sites)) --N_sites $N_sites --train_box 10 --epochs 5000 --deflate 1 --width 800        
     python alnls_involution.py --num_Hs $((2*N_sites)) --N_sites $N_sites --train_box 10000 --epochs 5000 --deflate 1 --width 800
 done
 
@@ -7,5 +8,6 @@ done
 
 for N_sites in {3..10} #3 4 5 6 7 8 9 10
 do
+    python alnls_involution.py --num_Hs $((2*N_sites)) --N_sites $N_sites --train_box 10 --epochs 5000 --deflate .5 --width 800        
     python alnls_involution.py --num_Hs $((2*N_sites)) --N_sites $N_sites --train_box 10000 --epochs 5000 --deflate .5 --width 800
 done
