2504fff2f6035e66191b9718c9aa8d993c27fdf0
WeiZhu <mrzw731@gmail.com>
2023-01-19 07:55:30 -0500

    plot figures

--- toda/experiment.sh
+++ toda/experiment.sh
@@ -1,11 +1,11 @@
 for N_sites in {3..10} #3 4 5 6 7 8 9 10
 do
-    python toda_involution.py --num_Hs $((2*N_sites)) --N_sites $N_sites --train_box 10 --epochs 5000 --deflate 1 --width 800
+    python toda_involution.py --num_Hs $((2*N_sites)) --N_sites $N_sites --train_box 50 --epochs 5000 --deflate 1 --width 800
 done
 
 
 
 for N_sites in {3..10} #3 4 5 6 7 8 9 10
 do
-    python toda_involution.py --num_Hs $((2*N_sites)) --N_sites $N_sites --train_box 10 --epochs 5000 --deflate .5 --width 800
+    python toda_involution.py --num_Hs $((2*N_sites)) --N_sites $N_sites --train_box 50 --epochs 5000 --deflate .5 --width 800
 done
