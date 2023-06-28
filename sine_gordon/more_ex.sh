83394301140a9729c200664df44753fe9a851920
WeiZhu <mrzw731@gmail.com>
2023-06-29 06:18:12 +0800

    fix bug

--- sine_gordon/more_ex.sh
+++ sine_gordon/more_ex.sh
@@ -1,6 +1,6 @@
 # large box = 100000
 
-python correlation.py --num_Hs 1 --N_sites 3 --train_box 10000 --epochs 15000 --deflate 1 --width 800 --epochs 10
+python correlation.py --num_Hs 1 --N_sites 3 --train_box 10000 --epochs 15000 --deflate 1 --width 800
 
 
 
